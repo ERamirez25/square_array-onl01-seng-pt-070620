79558e999bed843a372c4657e0e5253d660ac12e
Elaine Ramirez <elaineramirez208@yahoo.com>
2020-07-14 16:55:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,9 +2,9 @@ def square_array(array)
 end 
 
 def square_array(numbers)
-  new_array= []
-  array.each do |array|
-    new_array << [1, 4, 9]
+  new_numbers= []
+  new_numbers.each do |num|
+    new_numbers << [1, 4, 9]
   end 
 end 
   
